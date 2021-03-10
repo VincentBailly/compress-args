f27ed32150631e8278dfbf32f7564d8ff4a392e8
Vincent Bailly <vibailly@microsoft.com>
2021-03-10 10:40:11 +0100

    input validation

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 exports.compressArgs = (args) => {
-    if (args === undefined) {
-        throw new Error("CompressArgs expects one argument but its has been called with an undefined first argument.")
+    if (typeof args !== "object") {
+        throw new Error(`CompressArgs expects one argument of type object but got an arg of type "${typeof args}"`)
     }
+
+    if (!Array.isArray(args)) {
+        throw new Error("CompressArgs expects an array as argument but got an object which is not an array");
+    }
+
+    if (args.length === 0) {
+        throw new Error("ComressArgs expects a non-empty array as argument, but was called with one.")
+    }
+
+    args.forEach(e => {
+        if (typeof e !== "string") {
+            throw new Error("CompressArgs expects a string[] as argument, but got an array with other types in it.")
+        }
+    })
 
 }
--- test.js
+++ test.js
@@ -1,16 +1,59 @@
 const { compressArgs } = require(".");
 
-const throws = (func, message) => {
-    let threw = false;
-    try {
-        func();
-    } catch {
-        threw = true;
-    }
-    if (!threw) {
-        throw new Error(message);
-    }
-}
+const throws = (func) => {
+  let threw = false;
+  try {
+    func();
+  } catch {
+    threw = true;
+  }
+  if (!threw) {
+    throw new Error("Test failed.");
+  }
+};
 
-// Throws if args are empty
-throws(() => compressArgs(), "It should fail when called without arg");
+try {
+    
+  console.log("- compressArgs is a function");
+  if (typeof compressArgs !== "function") {
+    throw new Error("");
+  }
+
+  /**
+   * Input validation
+   */
+  console.log("- It should fail when called without arg.");
+  throws(() => compressArgs());
+
+  console.log("- It should fail when called with a number.");
+  throws(() => compressArgs(42));
+
+  console.log("- It should fail when called with a boolean.");
+  throws(() => compressArgs(true));
+
+  console.log("- It should fail when called with a bigInt.");
+  throws(() => compressArgs(42n));
+
+  console.log("- It should fail when called with a string.");
+  throws(() => compressArgs("a string"));
+
+  console.log("- It should fail when called with a function.");
+  throws(() => compressArgs(compressArgs));
+
+  console.log("- It should fail when called with a symbol.");
+  throws(() => compressArgs(new Symbol("foo")));
+
+  console.log("- It should fail when called with a non-array object.");
+  throws(() => compressArgs({ an: "object" }));
+
+  console.log("- It should fail when called with an empty array.");
+  throws(() => compressArgs([]));
+
+  console.log(
+    "- It should fail when called with an array array having something else than strings."
+  );
+  throws(() => compressArgs(["hello", { world: "!" }]));
+} catch {
+  console.error("Test failed.");
+  process.exit(1);
+}
